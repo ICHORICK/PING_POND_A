643e8796ed5ac667432d4e7db01491513a7ecdd1
ICHORICK <84141646+ICHORICK@users.noreply.github.com>
2023-05-28 17:21:56 +0300

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -8,7 +8,7 @@ window = display.set_mode ((win_width, win_height))
 window.fill(back)
 
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_y,player_x, player_speed,width,height):
+    def __init__(self, player_image, player_y, player_speed,width,height):
         super().__init__()
 
         self.image = transform.scale(image.load(player_image), (width, height))
@@ -36,11 +36,11 @@ class Player(GameSprite):
 #3
 while game:
     for e in event.get():
-        if e.type = QUIT:
+        if e.type == QUIT:
         game = False 
     if finish != True:
         window.fill(back)
-        racket1.update_1()
+        racket1.update_l()
         racket2.update_r()
         ball.rect.x += speed_x
         ball.rect.y += speed y
